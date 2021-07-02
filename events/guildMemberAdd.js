228b9a936739a78168f41892c09b1ed065001af4
yo-louk <loukrijnders@gmail.com>
2021-07-02 12:42:30 +0200

    moved welcome message to config.json

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -8,10 +8,8 @@ module.exports = {
 		let welcomeMessage = null;
 		if (guildMember.guild.id !== guild.id) return;
 		if (!guildMember.user.bot) {
-			const welcomeEmbed = new Discord.MessageEmbed()
-				.addField('Welkom in Révolution!', '• Lees de <#731206630112231464>.\n• Blijf up to date met Révolution in <#599235313617076235>.\n• Bekijk aankomende events in <#767461189928091678>.\n• Voor extra Brawl Stars roles, stuur een screenshot van je account in <#599241180466774016>.')
+			const welcomeEmbed = guild.welcomeEmbed
 				.setAuthor(guildMember.displayName, guildMember.user.displayAvatarURL())
-				.setThumbnail('https://i.imgur.com/erS7Qt6.gif')
 				.setColor(guild.color);
 			welcomeMessage = await client.channels.cache.get(guild.mainChannelID).send(`||${guildMember.toString()}||`, welcomeEmbed);
 		}
