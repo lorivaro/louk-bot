bbe2af387649901e8900ae59dd983636b8ad96f7
yo-louk <loukrijnders@gmail.com>
2021-07-03 16:14:20 +0200

    message categories

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js');
-const { guild } = require('../config.json');
+const { guild } = require('../../config.json');
 
 module.exports = {
     name: 'ping',
-    description: 'Ping!',
-    allowDM: true,
+    description: 'Measures the time takes to receive a message.',
+    global: true,
     execute(message) {
       const pingEmbed = new Discord.MessageEmbed().setDescription(`🏓 Pong! \`${(Date.now() - message.createdTimestamp)} ms\``).setColor(guild.color);
       return message.channel.send(pingEmbed);
--- events/message.js
+++ events/message.js
@@ -1,30 +1,41 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-const { bot } = require('../config.json');
+const { bot, guild } = require('../config.json');
 const client = require('../index.js');
 client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-  const command = require(`../commands/${file}`);
-  client.commands.set(command.name, command);
+const commandFolders = fs.readdirSync('./commands');
+for (const folder of commandFolders) {
+	const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const command = require(`../commands/${folder}/${file}`);
+		client.commands.set(command.name, command);
+	}
 }
 
 module.exports = {
   name: 'message',
   execute(message) {
-  if (!message.content.startsWith(bot.prefix) || message.author.bot) return;
+		if (!message.content.startsWith(bot.prefix) || message.author.bot) return;
+		const args = message.content.slice(bot.prefix.length).trim().split(/ +/);
+		const commandName = args.shift().toLowerCase();
+		const command = client.commands.get(commandName);
 
-	const args = message.content.slice(bot.prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
+		if (!command) return;
+		if (!message.guild || message.guild.id !== guild.id) {
+			if (!command.global) {
+				if (message.author.id !== bot.ownerID) return;
+			}
+    }
 
-	if (!client.commands.has(command)) return;
-
-	try {
-		client.commands.get(command).execute(message, args);
-	} catch (error) {
-		console.error(error);
-		message.reply('there was an error trying to execute that command!');
-	}
+		try {
+			command.execute(message, args);
+		} catch (error) {
+			console.error(error);
+			message.channel.send(new Discord.MessageEmbed()
+				.setTitle('Error')
+				.setColor('RED')
+				.setDescription(`An error occured whilst executing the \`${commandName}\` command:\n\`\`\`${error.message}\`\`\``));
+		}
   },
 };
