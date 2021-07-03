e31683b5e3927401b6f18d4236cf6027bd2c3397
yo-louk <loukrijnders@gmail.com>
2021-07-04 00:52:29 +0200

    typo

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -3,7 +3,7 @@ const { guild } = require('../../config.json');
 
 module.exports = {
     name: 'ping',
-    description: 'Measures the time takes to receive a message.',
+    description: 'Measures the time it takes to receive a message.',
     global: true,
     execute(message) {
       const pingEmbed = new Discord.MessageEmbed().setDescription(`🏓 Pong! \`${(Date.now() - message.createdTimestamp)} ms\``).setColor(guild.color);
