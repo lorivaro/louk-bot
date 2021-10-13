b770a85b412566a69cdf55360e82643e8b881417
yo-louk <loukrijnders@gmail.com>
2021-10-13 23:20:59 +0200

    log bots guildMemberAdd

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,19 +4,19 @@ const { guild } = require('../config.json');
 module.exports = {
 	name: 'guildMemberAdd',
 	async execute(guildMember, client) {
-		let welcomeMessage = null;
 		if (guildMember.guild.id !== guild.id) return;
-		if (guildMember.user.bot) return;
-
-		let welcomeEmbed = new Discord.MessageEmbed()
-			.setAuthor(guildMember.displayName, guildMember.user.displayAvatarURL())
-			.setColor(guild.color);
-		welcomeEmbed = Object.assign(welcomeEmbed, guild.welcomeEmbed);
-		welcomeMessage = await client.channels.cache.get(guild.mainChannelID).send({ content: `||${guildMember.toString()}||`, embeds: [welcomeEmbed] });
+		let welcomeMessage = null;
+		if (!guildMember.user.bot) {
+			let welcomeEmbed = new Discord.MessageEmbed()
+				.setAuthor(guildMember.displayName, guildMember.user.displayAvatarURL())
+				.setColor(guild.color);
+			welcomeEmbed = Object.assign(welcomeEmbed, guild.welcomeEmbed);
+			welcomeMessage = await client.channels.cache.get(guild.mainChannelID).send({ content: `||${guildMember.toString()}||`, embeds: [welcomeEmbed] });
+		}
 
 		const createdAt = guildMember.user.createdTimestamp.toString().slice(0, -3);
 		const joinEmbed = new Discord.MessageEmbed()
-			.setAuthor(guildMember.user.tag, guildMember.user.displayAvatarURL(), welcomeMessage.url)
+			.setAuthor(guildMember.user.tag, guildMember.user.displayAvatarURL(), welcomeMessage ? welcomeMessage.url : null)
 			.setDescription(`${guildMember.user.toString()} joined \n**Created:** <t:${createdAt}:d>\n**ID:** ${guildMember.user.id}`)
 			.setColor('GREEN');
 		client.channels.cache.get(guild.inviteChannelID).send({ embeds: [joinEmbed] });
